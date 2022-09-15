47846eaff85517f67e2f31ba3615c2d20adf1bc4
Abdiel Corro <acorrop@outlook.com>
2022-09-14 22:51:55 -0500

    exercise 2 done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // Ex. 1
+            // Exercise 1
 
             //Name: 
             //Favorite Color: 
@@ -14,7 +14,8 @@
             Console.WriteLine("Hello! What is your name?");
             var name = Console.ReadLine();
 
-            Console.WriteLine($"Hi {name}! Please answer the following questions in order to complete your profile: What is your favorite color?");
+            Console.WriteLine($"Hi {name}! Please answer the following questions in order to complete your profile:");
+                Console.WriteLine($"What is your favorite color?");
             var color = Console.ReadLine();
 
             Console.WriteLine($"{color}, great! Now, what is your favorite animal?");
@@ -31,15 +32,65 @@
             Console.WriteLine($"Favorite color: {color}");
             Console.WriteLine($"Favorite animal: {animal}");
             Console.WriteLine($"Favorite band: {band}");
-        
 
-            // Bonus * fun story
+
+            // Bonus * fun story *
+
             Console.WriteLine($"Here is a customized story for you {name}:");
 
             Console.WriteLine($"There was only half a {animal} in the apple. At first, {name} didn't quite " +
                 $"comprehend what this meant. \"Why would only half a {animal} be living in an apple?\" " +
                 $"{name} turned {color} from embarrassment and quickly spit out the last bite expecting " +
                 $"to see the other half of the {animal}. Suddenly, the band: {band} staring playing \"Meat is Murder...\".");
+
+
+            // Exercise 2
+
+            // Sum
+
+            Console.WriteLine($"Now, is boasting time {name}! let\'s find out about your programmign skills:");
+
+            Console.WriteLine("How many front-end languages do you konw?");
+             int frontEnd = int.Parse(Console.ReadLine()!);
+
+             Console.WriteLine("How many back-end languages do you know?");
+            int backEnd = int.Parse(Console.ReadLine()!);
+
+            
+            int languagesTotal = Sum(frontEnd, backEnd);
+            Console.WriteLine($"You have the power of knowing {languagesTotal} programming languages!");
+
+                              // Multiply
+
+            Console.WriteLine($"Finally, let\'s calculate the volume of your favority room in your house (in feet): ");
+
+            Console.WriteLine("What is the length?");
+            int length = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("What is the width?");
+            int width = int.Parse(Console.ReadLine()!);
+
+
+            Console.WriteLine("Last, what is the height?");
+            int height = int.Parse(Console.ReadLine()!);
+
+
+            int volumeTotal = Multiply(length, width, height);
+            Console.WriteLine($"The volume of your favorite room is: {volumeTotal} feets.");
+
+
         }
-    }
+
+
+              // Exercise 2
+
+        public static int Sum(int frontEnd, int backEnd)
+        {
+            return frontEnd + backEnd;
+        }
+        public static int Multiply(int length, int width, int height)
+        {
+            return length * width / height;
+        }
+} 
 }
